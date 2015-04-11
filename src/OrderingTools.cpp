51b85d54935913e34638a11fbc51f79c1ea2544e
Darren Strash <darren.strash@gmail.com>
2015-04-11 15:01:28 +0200

    Can do MISR ordering with isolates class

--- src/OrderingTools.cpp
+++ src/OrderingTools.cpp
@@ -629,6 +629,7 @@ void OrderingTools::InitialOrderingMISR(vector<vector<int>> const &adjacencyArra
 }
 #endif //0
 
+#if 0
 template <typename IsolatesType>
 void OrderingTools::InitialOrderingMISR(vector<vector<int>> const &adjacencyArray, IsolatesType const &isolates, vector<int> &vOrderedVertices, vector<int> &vColoring, size_t &cliqueSize)
 {
@@ -863,6 +864,256 @@ void OrderingTools::InitialOrderingMISR(vector<vector<int>> const &adjacencyArra
 #endif // 0
     }
 }
+#else
+template <typename IsolatesType>
+void OrderingTools::InitialOrderingMISR(vector<vector<int>> const &adjacencyArray, IsolatesType const &isolates, vector<int> &vOrderedVertices, vector<int> &vColoring, size_t &cliqueSize)
+{
+    size_t const numVertices(isolates.GetInGraph().Size());
+    vOrderedVertices.resize(numVertices, -1);
+    vColoring.resize(numVertices, -1);
+
+    size_t const size(adjacencyArray.size());
+    vector<bool> vMarkedVertices(size);
+
+    // array of lists of vertices, indexed by degree
+    vector<list<int>> verticesByDegree(size);
+
+    // array of lists of vertices, indexed by degree
+    vector<list<int>::iterator> vertexLocator(size);
+
+    vector<int> coDegree(size);
+
+    // fill each cell of degree lookup table
+    // then use that degree to populate the 
+    // lists of vertices indexed by degree
+
+    size_t maxDegree(0);
+    size_t maxCoDegree(0);
+    size_t uSumOfCoDegrees(0);
+    for(int const i : isolates.GetInGraph()) {
+        coDegree[i] = numVertices - isolates.Neighbors()[i].Size() - 1;
+////        vertexLocator[i] = verticesByDegree[coDegree[i]].insert(verticesByDegree[coDegree[i]].end(), i);
+        verticesByDegree[coDegree[i]].push_front(i);
+        vertexLocator[i] = verticesByDegree[coDegree[i]].begin();
+
+        maxDegree = max(maxDegree, isolates.Neighbors()[i].Size());
+        maxCoDegree = max(maxCoDegree, static_cast<size_t>(coDegree[i]));
+        uSumOfCoDegrees += coDegree[i];
+    }
+
+    // perform degeneracy ordering in complement graph.
+
+    int currentDegree = 0;
+    int numVerticesRemoved = 0;
+
+    vector<NeighborListArray> vOrderingArray(size);
+
+    while (numVerticesRemoved < numVertices) {
+////        cout << "Ordered " << numVerticesRemoved << "/" << numVertices << " vertices" << endl;
+        if (!verticesByDegree[currentDegree].empty()) {
+
+            int vertex(-1);
+            if (verticesByDegree[currentDegree].size() > 1) {
+
+                // if remaining graph is regular.
+                if ((numVertices - numVerticesRemoved) == verticesByDegree[currentDegree].size()) {
+////                    cout << "Remaining graph of " << verticesByDegree[currentDegree].size() << " vertices is regular with degree " << currentDegree << endl;
+////                    cout << "Vertices: ";
+////                    for (int const lastVertex : verticesByDegree[currentDegree]) {
+////////                        if (vColoring.size() - index < 10)
+////                            cout << lastVertex << " ";
+////                    }
+////                    cout << endl;
+
+                    // if regular, and degree is # vertices - 1, then it's a clique.
+                    if (static_cast<int>(verticesByDegree[currentDegree].size()) == (currentDegree + 1)) { // TODO/DS put in the proper check for independent set...
+                        if (cliqueSize < verticesByDegree[currentDegree].size())
+                            cliqueSize = verticesByDegree[currentDegree].size();
+                    }
+
+                    vector<int> remainingVertices(verticesByDegree[currentDegree].begin(), verticesByDegree[currentDegree].end());
+                    vector<int> remainingColors(remainingVertices.size(), 0);
+////                    CliqueColoringStrategy coloringStrategy(adjacencyMatrix);
+                    SparseIndependentSetColoringStrategy coloringStrategy(adjacencyArray); // TODO/DS: Validate.
+                    coloringStrategy.Color(adjacencyArray, remainingVertices /* evaluation order */, remainingVertices /* color order */, remainingColors);
+                    //copy initial ordering to output arrays
+
+////                    int maxColor(0);
+////                    size_t index(0);
+                    for (size_t index = 0; index < remainingVertices.size(); ++index) {
+                        vOrderedVertices[index] = remainingVertices[index];
+                        vColoring[index] = remainingColors[index];
+////                        maxColor = max(maxColor, vColoring[index]);
+                    }
+
+#if 1
+                    // simpler
+                    for (size_t index = remainingColors.size(); index < vColoring.size(); ++index) {
+                        vColoring[index] = min(vColoring[index-1] + 1, static_cast<int>(maxCoDegree + 1));
+                    }
+
+////                    cout << "All vertices: ";
+////                    for (size_t index = 0; index < vOrderedVertices.size(); ++index) {
+////////                        if (vColoring.size() - index < 10)
+////                            cout << vOrderedVertices[index] << " ";
+////                    }
+////                    cout << endl;
+////                    cout << "All colors: ";
+////                    for (size_t index = 0; index < vColoring.size(); ++index) {
+////////                        if (vColoring.size() - index < 10)
+////                            cout << vColoring[index] << " ";
+////                    }
+////                    cout << endl;
+#else
+                    int const lastIndexWithSmallerColor(min(remainingVertices.size() + maxDegree - maxColor, numVertices-1));
+                    int currentColor = maxColor + 1;
+                    while (index <= lastIndexWithSmallerColor) {
+                        vColoring[index] = currentColor;
+                        currentColor++;
+                        index++;
+                    }
+                    while (index < numVertices) {
+                        vColoring[index] = maxDegree + 1;
+                        index++;
+                    }
+#endif //0
+
+#if 0
+                    cout << "Snippet of ordering: ";
+                    for (size_t index = 0; index < vOrderedVertices.size(); ++index) {
+#ifdef SNIPPET
+                        if (index == 10 && vOrderedVertices.size() > 21) {
+                            cout << "...";
+                            index = vOrderedVertices.size() - 10;
+                        }
+#endif // SNIPPET
+
+                        cout << vOrderedVertices[index] << " ";
+                    }
+                    cout << endl;
+#endif
+
+                    return;
+                } else {
+                    // break ties by neighborhood-degree
+                    size_t minNeighborhoodDegree(ULONG_MAX);
+////                    cout << "currentDegree= " << currentDegree << endl << flush;
+                    int    chosenVertex=verticesByDegree[currentDegree].front();
+                    for (int const candidate : verticesByDegree[currentDegree]) {
+                        size_t coNeighborhoodDegree(0);
+#if 0
+                        for (int const neighbor : isolates.Neighbors()[candidate]) {
+                            vMarkedVertices[neighbor] = true;
+                        }
+
+                        // iterate over all non-neighbors, and add up their non neighbors
+                        for (int nonNeighbor = 0; nonNeighbor < numVertices; ++nonNeighbor) {
+                            if (coDegree[nonNeighbor] != -1 && !vMarkedVertices[nonNeighbor]) {
+                                coNeighborhoodDegree += (coDegree[nonNeighbor]);
+                            }
+                        }
+                        // verify coDegree by difference, TODO/DS: Remove for production
+#if 0
+                        size_t uSizeNeighborhoodCoDegrees(0);
+                        for (int const neighbor : isolates.Neighbors()[candidate]) {
+                            uSizeNeighborhoodCoDegrees += coDegree[neighbor];
+                        }
+                        size_t uVerify2CoNeighborhood(uSumOfCoDegrees - uSizeNeighborhoodCoDegrees);
+                        if (uVerify2CoNeighborhood != coNeighborhoodDegree) {
+                            cout << "DEBUG: incremental 2-coNeighborhood " << uVerify2CoNeighborhood << " is not equal to the real value: " << coNeighborhoodDegree << endl << flush;
+                        }
+#endif // 0
+
+                        for (int const neighbor : isolates.Neighbors()[candidate]) {
+                            vMarkedVertices[neighbor] = false;
+                        }
+#else
+                        size_t uSizeNeighborhoodCoDegrees(0);
+                        for (int const neighbor : adjacencyArray[candidate]) {
+                            if (coDegree[neighbor] != -1)
+                                uSizeNeighborhoodCoDegrees += coDegree[neighbor];
+                        }
+
+                        coNeighborhoodDegree = (uSumOfCoDegrees - uSizeNeighborhoodCoDegrees);
+////                        if (uVerify2CoNeighborhood != coNeighborhoodDegree) {
+////                            cout << "DEBUG: incremental 2-coNeighborhood " << uVerify2CoNeighborhood << " is not equal to the real value: " << coNeighborhoodDegree << endl << flush;
+////                        }
+#endif // 0
+                        if (coNeighborhoodDegree < minNeighborhoodDegree || (coNeighborhoodDegree == minNeighborhoodDegree && candidate > chosenVertex)) {
+                            minNeighborhoodDegree = coNeighborhoodDegree;
+                            chosenVertex = candidate;
+                        }
+                    }
+                    vertex = chosenVertex;
+                    verticesByDegree[currentDegree].erase(vertexLocator[vertex]);
+////                    cout << "Choosing multi vertex: " << vertex << " with degree " << coDegree[vertex]  << " and neighborhood " << minNeighborhoodDegree << endl << flush;
+                }
+            } else {
+                vertex = verticesByDegree[currentDegree].front();
+                verticesByDegree[currentDegree].pop_front();
+////                cout << "Choosing solo  vertex: " << vertex << " with degree " << coDegree[vertex] << endl << flush;
+            }
+
+            vOrderedVertices[vOrderedVertices.size() - numVerticesRemoved - 1] = vertex;
+
+            uSumOfCoDegrees -= 2*coDegree[vertex];
+            coDegree[vertex] = -1;
+
+#if 0
+            for (int const neighbor : adjacencyArray[vertex]) {
+                if (coDegree[neighbor] > -1)
+                {
+                    verticesByDegree[coDegree[neighbor]].erase(vertexLocator[neighbor]);
+                    coDegree[neighbor]--;
+                    verticesByDegree[coDegree[neighbor]].push_front(neighbor);
+                    vertexLocator[neighbor] = verticesByDegree[coDegree[neighbor]].begin();
+                }
+            }
+#else
+            // need to iterate over non-neighbors
+            for (int const neighbor : isolates.Neighbors()[vertex]) {
+                vMarkedVertices[neighbor] = true;
+            }
+
+            // iterate over all non-neighbors, subtract from their co-Degree
+            for (int const nonNeighbor : isolates.GetInGraph()) { //// = 0; nonNeighbor < numVertices; ++nonNeighbor) {
+                if (coDegree[nonNeighbor] != -1 && !vMarkedVertices[nonNeighbor]) {
+                    verticesByDegree[coDegree[nonNeighbor]].erase(vertexLocator[nonNeighbor]);
+                    coDegree[nonNeighbor]--;
+////                    vertexLocator[nonNeighbor] = verticesByDegree[coDegree[nonNeighbor]].insert(verticesByDegree[coDegree[nonNeighbor]].end(), nonNeighbor);
+                    verticesByDegree[coDegree[nonNeighbor]].push_front(nonNeighbor);
+                    vertexLocator[nonNeighbor] = verticesByDegree[coDegree[nonNeighbor]].begin();
+                }
+            }
+
+            for (int const neighbor : isolates.Neighbors()[vertex]) {
+                vMarkedVertices[neighbor] = false;
+            }
+
+#endif // 0
+            numVerticesRemoved++;
+            currentDegree--; // degrees can't grow...
+            if (currentDegree < 0) currentDegree = 0;
+       } else {
+            currentDegree++;
+        }
+// verify new sum: TODO/DS: remove for production...
+#if 0
+            size_t verifySum(0);
+            for (int const value : coDegree) {
+                if (value != -1) {
+                    verifySum+=value;
+                }
+            }
+
+            if (uSumOfCoDegrees != verifySum) {
+                cout << "DEBUG: incremental sum " << uSumOfCoDegrees << " does not match real sum " << verifySum << endl << flush;
+            }
+#endif // 0
+    }
+}
+
+#endif // 0
 
 void OrderingTools::InitialOrderingMISR(vector<vector<char>> const &adjacencyMatrix, vector<int> &vOrderedVertices, vector<int> &vColoring, size_t &cliqueSize)
 {
@@ -1011,6 +1262,9 @@ void OrderingTools::InitialOrderingReduction<Isolates3<ArraySet>>(Isolates3<Arra
 
 template
 void OrderingTools::InitialOrderingMISR<Isolates4<SparseArraySet>>(std::vector<std::vector<int>> const &adjacencyArray, Isolates4<SparseArraySet> const &isolates, std::vector<int> &vOrderedVertices, std::vector<int> &vColoring, size_t &cliqueSize);
+
+template
+void OrderingTools::InitialOrderingMISR<Isolates4<ArraySet>>(std::vector<std::vector<int>> const &adjacencyArray, Isolates4<ArraySet> const &isolates, std::vector<int> &vOrderedVertices, std::vector<int> &vColoring, size_t &cliqueSize);
 
 template
 void OrderingTools::InitialOrderingMISR<Isolates3<ArraySet>>(std::vector<std::vector<int>> const &adjacencyArray, Isolates3<ArraySet> const &isolates, std::vector<int> &vOrderedVertices, std::vector<int> &vColoring, size_t &cliqueSize);
